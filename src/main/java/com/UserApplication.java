aedc75fe21991689fac309fac4238dc3b81c4237
Vishal <vishal007gohan@gmail.com>
2021-05-04 13:49:58 +0530

    Actuator and Spring Admin

--- src/main/java/com/UserApplication.java
+++ src/main/java/com/UserApplication.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
+
 public class UserApplication {
 
 	public static void main(String[] args) {
